3913259a738ff146aa772ab05bb1bf4be1b90e51
ZhouZhengCarlos <carloszhou0@gmail.com>
2020-03-16 21:34:11 -0600

    La wea del nombre creo xdxd

--- src/java/servlets/RetireServlet.java
+++ src/java/servlets/RetireServlet.java
@@ -16,7 +16,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Account;
+import model.Currency;
 import model.Transfer;
+import services.CurrencyService;
 
 /**
  *
@@ -26,27 +28,62 @@ import model.Transfer;
         name = "RetireServlet",
         urlPatterns = {"/Retire"}
 )
-public class RetireServlet  extends HttpServlet{
-      private void processRequest(
+public class RetireServlet extends HttpServlet {
+
+    private void processRequest(
             HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         AccountService accountservice = new AccountService();
         TransferService transferservice = new TransferService();
+        CurrencyService currencyservice = new CurrencyService();
         Optional<Account> aux = null;
         aux = accountservice.getAccount(request.getParameter("numcuenta"));
+        double balance1 = Double.parseDouble(request.getParameter("monto"));
+        String typeCurrency = request.getParameter("cambio");
+        Optional<Currency> currency = currencyservice.getCurrency(request.getParameter("cambio"));
         double balance = aux.get().getBalance();
-        double sub = balance - Double.parseDouble(request.getParameter("monto"));
-        accountservice.updateAllAccount(aux.get().getId(), aux.get().getTransactionLimit(), aux.get().getIsActive(), sub);
-        Transfer transaux = null;
-        
-        java.util.Date date = new java.util.Date();
-        java.sql.Date sqlDate = new java.sql.Date(date.getTime());
-        
-        transaux = new Transfer(0, aux.get().getId(), 1, sub, sqlDate);
-        transferservice.createTransfer(transaux);
+        //PREGUNTAR SI TIENE PLATA
+
+        //MONEDA
+        if (aux.get().getCurrencyName().equals("CRC")) {//SI LA CUENTA ES COLONES
+            if ("USD".equals(typeCurrency)) {
+                balance1 = balance1 * 560;
+            } else if ("EUR".equals(typeCurrency)) {
+                balance1 = balance1 * 700;
+            }
+        } else if ("USD".equals(aux.get().getCurrencyName())) { //SI LA CUENTA ES DOLARES
+            if ("CRC".equals(typeCurrency)) {
+                balance1 = balance1 / 570;
+            } else if ("EUR".equals(typeCurrency)) {
+                balance1 = balance1 / 0.89;
+            }
+        } else if ("EUR".equals(aux.get().getCurrencyName())) { //SI LA CUENTA ES EUROS
+            if ("USD".equals(typeCurrency)) {
+                balance1 = balance1 / 1.12;
+            } else if ("CRC".equals(typeCurrency)) {
+                balance1 = balance1 / 0.0016;
+            }
+        }
+
+        double sub = balance - balance1;
+        if (sub >= 0) {
+            if (sub != 0) {
+                accountservice.updateAllAccount(aux.get().getId(), aux.get().getTransactionLimit(), aux.get().getIsActive(), sub);
+            } else {
+                accountservice.updateAllAccount(aux.get().getId(), aux.get().getTransactionLimit(), 0, sub);
+            }
+            Transfer transaux = null;
+
+            java.util.Date date = new java.util.Date();
+            java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+
+            transaux = new Transfer(0, aux.get().getId(), 1, sub, sqlDate);
+            transferservice.createTransfer(transaux);
+        }
         RequestDispatcher dispatcher = request.getRequestDispatcher(
                 "retiremenu.jsp");
         dispatcher.forward(request, response);
+
     }
 
     @Override
