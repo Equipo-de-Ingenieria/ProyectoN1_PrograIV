81f09c1df12ccfb4824323f8c765b1adb054cf32
ZhouZhengCarlos <carloszhou0@gmail.com>
2020-03-16 19:11:46 -0600

    Credit Servlet changes

--- src/java/servlets/CreditServlet.java
+++ src/java/servlets/CreditServlet.java
@@ -5,14 +5,17 @@
  */
 package servlets;
 
-
 import java.io.IOException;
+import java.util.Optional;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Account;
+import model.AccountType;
 import services.AccountService;
+import services.AccountTypeService;
 
 /**
  *
@@ -21,12 +24,18 @@ import services.AccountService;
 @WebServlet(
         name = "CreditServlet",
         urlPatterns = {"/CreditServlet"})
-public class CreditServlet extends HttpServlet{
-       private void processRequest(
+public class CreditServlet extends HttpServlet {
+
+    private void processRequest(
             HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-             AccountService accountservice = new AccountService();
-        
+        AccountService accountservice = new AccountService();
+        AccountTypeService typeservice = new AccountTypeService();
+        Optional<Account> account = accountservice.getAccount(request.getParameter("cuenta"));
+        Optional<AccountType> type = typeservice.getAccount(request.getParameter(account.get).getAccountTypeID());
+        double interests = type.get().getInterest() * account.get().getBalance();
+        double sum = interests + account.get().getBalance();
+
     }
 
     @Override
